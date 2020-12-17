4ff40f901c3d5752c692f562cd79cb2748d2f6a6
Edward <e.arechwa@gmail.com>
2020-12-17 13:50:52 +0100

    add gh-pages

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-const menuTrigger = document.querySelector('.header__hamburger');
-const menu = document.querySelector('.left-sidebar');
-
-menuTrigger.addEventListener('click', () => {
-	menu.classList.toggle('active');
-});
